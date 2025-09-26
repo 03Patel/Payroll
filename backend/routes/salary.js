e92c2b91f01d7eb9556fcd629e327d0193b02333
Ganesh  ji patel <ganeshjipatel108@gmail.com>
2025-09-26 18:09:53 +0530

    up

--- backend/routes/salary.js
+++ backend/routes/salary.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const SalarySlip = require('../models/SalarySlip');
+const SalarySlip = require('../models/salarySlip');
 const authMiddleware = require('../middleware/auth');
 
 // --- Create Salary Slip (Admin only) ---
